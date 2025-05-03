def98c255b2073060388089117a94790ec6784e1
RyeMatthews <anthonyjolly10@live.com>
2025-05-02 21:37:59 -0400

    Update quoteCalculator.js

--- src/utils/quoteCalculator.js
+++ src/utils/quoteCalculator.js
@@ -1,1 +1,15 @@
-0
+0// src/utils/quoteCalculator.js
+export function calculateQuote(volumeCm3, materialType) {
+  const baseServiceFee = 10;
+  const materialPrices = {
+    plastic: 0.25,
+    resin: 0.35,
+    metal: 1.5,
+    sandstone: 0.4,
+  };
+
+  const materialPrice = materialPrices[materialType] || materialPrices['plastic'];
+  const totalCost = (volumeCm3 * materialPrice) + baseServiceFee;
+
+  return totalCost.toFixed(2);
+}
